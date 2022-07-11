11288c870d5a7a013a00f14b3573cd4313e3caf8
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-07-11 13:45:21 +0000

    Trim down nix-shell in CI case

--- shell.nix
+++ shell.nix
@@ -1,8 +1,22 @@
-(import ./default.nix).shellFor {
-  tools = {
+{
+  # Used in CI to have a smaller closure
+  withoutDevTools ? false
+}:
+let
+  project = import ./default.nix ;
+
+  buildTools = {
     cabal = "latest";
+  };
+
+  devTools = if withoutDevTools then { } else {
     hlint = "latest";
     haskell-language-server = "latest";
     fourmolu = "latest";
   };
-}
+
+  shell = project.shellFor {
+    tools =   buildTools // devTools;
+  };
+
+in shell { }
