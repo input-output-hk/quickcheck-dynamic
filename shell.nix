407e1482bc51d9289b832690c8cb26b6c0a53434
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-07-11 13:29:49 +0000

    Upgrade cabal version to latest

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 (import ./default.nix).shellFor {
   tools = {
-    cabal = "3.4.0.0";
+    cabal = "latest";
     hlint = "latest";
     haskell-language-server = "latest";
     fourmolu = "latest";
