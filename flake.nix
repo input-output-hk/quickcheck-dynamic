c54efebf4b03d97a6da58f11ae386cb84ab67da0
zeme <lorenzo.calegari@iohk.io>
2023-05-29 16:05:05 +0200

    upgrade ghc 9.2.7

--- flake.nix
+++ flake.nix
@@ -12,12 +12,11 @@
       repoRoot = ./.;
       flakeOutputsPrefix = "";
       systems = [ "x86_64-darwin" "x86_64-linux" ];
-      haskellCompilers = [ "ghc8107" ];
-      defaultHaskellCompiler = "ghc8107";
+      haskellCompilers = [ "ghc927" ];
+      defaultHaskellCompiler = "ghc927";
       haskellCrossSystem = null;
       haskellProjectFile = ./nix/haskell-project.nix;
       perSystemOutputsFile = null;
-      shellName = "quickcheck-dynamic";
       shellPrompt = "\n\\[\\033[1;31m\\][quickcheck-dynamic]\\$\\[\\033[0m\\] ";
       shellWelcomeMessage = "🤟 \\033[1;31mWelcome to quickcheck-dynamic\\033[0m 🤟";
       shellModuleFile = null;
@@ -25,7 +24,6 @@
       excludeProfiledHaskellFromHydraJobs = true;
       blacklistedHydraJobs = [ ];
       enableHydraPreCommitCheck = true;
-      includeReadTheDocsSite = false;
       readTheDocsSiteDir = null;
       readTheDocsHaddockPrologue = "";
       readTheDocsExtraHaddockPackages = null;
