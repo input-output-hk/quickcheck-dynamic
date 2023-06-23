5d183dd0055d3ad6b8690ab0be2a9d0a4e865503
zeme <lorenzo.calegari@iohk.io>
2023-06-23 11:46:56 +0200

    WIP

--- nix/pre-commit-check.nix
+++ nix/pre-commit-check.nix
@@ -1,5 +1,9 @@
 { ... }:
 {
+  cabal-fmt.enable = true; 
+
+  nixpkgs-fmt.enable = true;
+
   fourmolu.enable = true;
   fourmolu.extraOptions = "-o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms";
 }
