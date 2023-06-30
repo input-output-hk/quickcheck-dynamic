7d555f2060cd9e5585bfe1e72f179e79f1c4f7d9
zeme <lorenzo.calegari@iohk.io>
2023-06-30 11:15:17 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -2,14 +2,33 @@
   description = "QuickCheck Dynamic";
 
   inputs = {
-    iogx.url = "github:zeme-iohk/iogx";
+    iogx.url = "github:zeme-wana/iogx";
   };
 
   outputs = inputs:
     inputs.iogx.mkFlake {
       inherit inputs;
+      debug = true;
       repoRoot = ./.;
+      flakeOutputsPrefix = "";
+      systems = [ "x86_64-darwin" "x86_64-linux" ];
+      haskellCompilers = [ "ghc8107" ];
+      defaultHaskellCompiler = "ghc8107";
+      haskellCrossSystem = null;
+      haskellProjectFile = ./nix/haskell-project.nix;
+      perSystemOutputsFile = null;
       shellName = "quickcheck-dynamic";
+      shellPrompt = "\n\\[\\033[1;31m\\][quickcheck-dynamic]:\\w]\\$\\[\\033[0m\\] ";
+      shellWelcomeMessage = "🤟 \\033[1;31mWelcome to quickcheck-dynamic\\033[0m 🤟";
+      shellModuleFile = null;
+      includeHydraJobs = true;
+      excludeProfiledHaskellFromHydraJobs = true;
+      blacklistedHydraJobs = [ ];
+      enableHydraPreCommitCheck = true;
+      includeReadTheDocsSite = false;
+      readTheDocsSiteDir = null;
+      readTheDocsHaddockPrologue = "";
+      readTheDocsExtraHaddockPackages = null;
     };
 
   nixConfig = {
@@ -21,7 +40,7 @@
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
-    allow-import-from-derivation = "true";
+    accept-flake-config = true;
+    allow-import-from-derivation = true;
   };
-
 }
