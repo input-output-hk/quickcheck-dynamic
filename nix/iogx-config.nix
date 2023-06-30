edf2147b81b55487f8f020f7fff3705be0283081
zeme <lorenzo.calegari@iohk.io>
2023-06-30 10:19:17 +0200

    Upd

--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,13 +1,8 @@
-< < <<<<< Updated upstream
-{
-repoRoot = ../.;
-=======
 # This file is part of the IOGX template and is documented at the link below:
 # https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
 
 {
->>>>>>> Stashed changes
-systems = [ "x86_64-linux" "x86_64-darwin" ];
-haskellCompilers = [ "ghc927" ];
-shouldCrossCompile = false;
+  systems = [ "x86_64-linux" "x86_64-darwin" ];
+  haskellCompilers = [ "ghc927" ];
+  shouldCrossCompile = false;
 }
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,10 +1,6 @@
-< < <<<<< Updated upstream
-{ ... }:
-=======
 # This file is part of the IOGX template and is documented at the link below:
 # https://www.github.com/input-output-hk/iogx#34-nixshellnix
 
->>>>>>> Stashed changes
 {
-name = "quickcheck-dynamic";
+  name = "quickcheck-dynamic";
 }
