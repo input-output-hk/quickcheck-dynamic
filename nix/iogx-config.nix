06ebb76e4c3a92e3907b10b4ee289ca99d44b22e
zeme <lorenzo.calegari@iohk.io>
2023-06-30 10:17:21 +0200

    Bump IOGX to bc5f588

--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,6 +1,13 @@
+< < <<<<< Updated upstream
 {
-  repoRoot = ../.;
-  systems = [ "x86_64-linux" "x86_64-darwin" ];
-  haskellCompilers = [ "ghc927" ];
-  shouldCrossCompile = false;
+repoRoot = ../.;
+=======
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
+
+{
+>>>>>>> Stashed changes
+systems = [ "x86_64-linux" "x86_64-darwin" ];
+haskellCompilers = [ "ghc927" ];
+shouldCrossCompile = false;
 }
--- nix/pre-commit-check.nix
+++ nix/pre-commit-check.nix
@@ -1,8 +1,14 @@
+< < <<<<< Updated upstream
+=======
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#38-nixpre-commit-checknix
+
+>>>>>>> Stashed changes
 {
-  cabal-fmt.enable = true;
+cabal-fmt.enable = true;
 
-  nixpkgs-fmt.enable = true;
+nixpkgs-fmt.enable = true;
 
-  fourmolu.enable = true;
-  fourmolu.extraOptions = "-o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms";
+fourmolu.enable = true;
+fourmolu.extraOptions = "-o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms";
 }
