b50a995a1ed13dfad811d3483639603fd34ae8cf
Maximilian Algehed <MaximilianAlgehed@users.noreply.github.com>
2024-04-17 15:51:36 +0200

    remove Realized (#76)
    * remove Realized
    
    * remove unnecessary stuff
    
    * fmt
    
    * migration notes
    
    * cabal fmt

--- quickcheck-dynamic-iosim/src/Test/QuickCheck/StateModel/IOSim.hs
+++ quickcheck-dynamic-iosim/src/Test/QuickCheck/StateModel/IOSim.hs
@@ -2,28 +2,12 @@
 
 module Test.QuickCheck.StateModel.IOSim where
 
-import Control.Concurrent
-import Control.Concurrent.Class.MonadSTM.TMVar qualified as IOClass
-import Control.Concurrent.Class.MonadSTM.TVar qualified as IOClass
-import Control.Concurrent.STM
 import Control.Exception (SomeException (..))
-import Control.Monad.Class.MonadFork qualified as IOClass
 import Control.Monad.IOSim
 
 import Test.QuickCheck
 import Test.QuickCheck.Gen.Unsafe (Capture (Capture), capture)
 import Test.QuickCheck.Monadic
-import Test.QuickCheck.StateModel
-
-type family RealizeIOSim s a where
-  RealizeIOSim s ThreadId = IOClass.ThreadId (IOSim s)
-  RealizeIOSim s (TVar a) = IOClass.TVar (IOSim s) a
-  RealizeIOSim s (MVar a) = IOClass.TMVar (IOSim s) a
-  RealizeIOSim s (f a b) = f (RealizeIOSim s a) (RealizeIOSim s b)
-  RealizeIOSim s (f a) = f (RealizeIOSim s a)
-  RealizeIOSim s a = a
-
-type instance Realized (IOSim s) a = RealizeIOSim s a
 
 runIOSimProperty :: Testable a => (forall s. PropertyM (IOSim s) a) -> Gen (SimTrace Property, Property)
 runIOSimProperty p = do
--- quickcheck-dynamic/CHANGELOG.md
+++ quickcheck-dynamic/CHANGELOG.md
@@ -9,6 +9,16 @@ changes.
 
 ## UNRELEASED
 
+* Breaking removed `Realized`
+  - To migrate uses of `Realized` with `IOSim`, index the state type on the choice of `RunModel` monad
+    and index the relevant types:
+    ```
+    -- Turn:
+    data ModelState = State { threadId :: Var ThreadId }
+    -- Into:
+    data ModelState m = State { threadId :: Var (ThreadId m) }
+    ```
+
 ## 3.4.1 - 2024-03-22
 
 * [#70](https://github.com/input-output-hk/quickcheck-dynamic/pull/70) Expose `IsPerformResult` typeclass
