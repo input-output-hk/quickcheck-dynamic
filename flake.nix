03f85a3c1e5f17bb5a3036e036667a7e2d3c8c89
Robin Stumm <dermetfan@users.noreply.github.com>
2026-05-04 14:29:04 +0200

    drop support for x86_64-darwin (#102)

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
   outputs = inputs: inputs.iogx.lib.mkFlake {
     inherit inputs;
     repoRoot = ./.;
-    systems = [ "x86_64-darwin" "x86_64-linux" "aarch64-darwin" ];
+    systems = [ "x86_64-linux" "aarch64-darwin" ];
     outputs = import ./nix/outputs.nix;
   };
 
